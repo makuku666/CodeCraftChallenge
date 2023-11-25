d07b745961f1df267f8e714ac5a4af9276aeafa7
marcus k <marcus.koch01@outlook.de>
2023-11-25 16:22:37 +0100

    refactor: Vite Server Props

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { port: 3000, open: true, strictPort: true },
   input: 'src/index.js',
   output: {
     dir: 'dist',
