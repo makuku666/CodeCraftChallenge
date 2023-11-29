3fb537fbb73ca6d975e2b2ddc90469b837bd8b79
marcus k <marcus.koch01@outlook.de>
2023-11-29 17:13:27 +0100

    feat: Render content node

--- src/pages/ContentPage/ContentPage.jsx
+++ src/pages/ContentPage/ContentPage.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import { useEffect, useMemo } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useQuery } from '@apollo/client'
+import { nanoid } from 'nanoid'
+import LoadSpinner from 'src/components/LoadSpinner/LoadSpinner'
+import { ERR_CODE_UNAUTH } from 'src/constants/error.const'
+import { NAV_LOGIN } from 'src/constants/routeNames.const'
+import { GQL_CONTENT } from 'src/graphql/queries/content'
+import { getHeader } from 'src/utils/login.util'
 
 const ContentPage = () => {
-  return <div>ContentPage</div>
+  const { data, loading, error } = useQuery(GQL_CONTENT, getHeader())
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const statusCode = error?.networkError?.statusCode
+
+    if (statusCode === ERR_CODE_UNAUTH) {
+      navigate(NAV_LOGIN)
+    }
+  }, [error])
+
+  const contentList = useMemo(() => {
+    const temp = data?.Admin?.Tree?.GetContentNodes?.edges
+    if (!data || !temp) return []
+
+    return temp.map((content) => {
+      const { title } = content.node.structureDefinition
+
+      return (
+        <div key={nanoid()}>
+          <h3>{title}</h3>
+        </div>
+      )
+    })
+  }, [data])
+
+  return loading ? <LoadSpinner /> : contentList
 }
 
 export default ContentPage
