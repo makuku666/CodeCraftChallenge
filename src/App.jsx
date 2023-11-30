1303b17dcf8951e98f79172df334ab5025ba5078
marcus k <marcus.koch01@outlook.de>
2023-11-30 13:04:31 +0100

    feat: UI Lib - Chakra UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { Suspense } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { ApolloProvider } from '@apollo/client'
+import { ChakraProvider } from '@chakra-ui/react'
 import { apolloClient } from 'src/graphql/client'
 import { routerConfig } from 'src/routes/route'
 
 import LoadSpinner from 'components/LoadSpinner/LoadSpinner'
+import './App.css'
 
 const App = () => {
   return (
     <ApolloProvider client={apolloClient()}>
-      <Suspense fallback={<LoadSpinner />}>
-        <RouterProvider router={routerConfig} />
-      </Suspense>
+      <ChakraProvider>
+        <Suspense fallback={<LoadSpinner />}>
+          <RouterProvider router={routerConfig} />
+        </Suspense>
+      </ChakraProvider>
     </ApolloProvider>
   )
 }
