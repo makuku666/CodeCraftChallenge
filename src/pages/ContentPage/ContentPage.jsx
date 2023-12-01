f803b1f831c098020627bd2743f617069fa926cb
marcus <marcus.koch01@outlook.de>
2023-12-01 15:19:26 +0100

    feat: React Drag&Drop

--- src/pages/ContentPage/ContentPage.jsx
+++ src/pages/ContentPage/ContentPage.jsx
@@ -1,15 +1,23 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import { useNavigate } from 'react-router-dom'
 import { useLazyQuery } from '@apollo/client'
 import { Grid } from '@chakra-ui/react'
+import update from 'immutability-helper'
+import ContentTile from 'pages/ContentPage/components/ContentTile/ContentTile'
 import { ERR_CODE_UNAUTH } from 'src/constants/error.const'
 import { NAV_LOGIN } from 'src/constants/routeNames.const'
 import { GQL_CONTENT } from 'src/graphql/queries/content'
 import { getHeader } from 'src/utils/login.util'
 
-import ContentTile from 'components/ContentTile/ContentTile'
-
+/**
+ * ContentPage Component
+ *
+ * This component represents a page displaying a list of content tiles with draggable functionality.
+ * It uses Infinite Scroll to load more content and supports dragging tiles using React DnD.
+ */
 const ContentPage = () => {
   const [fetchContent, { data: lazyData, error }] = useLazyQuery(
     GQL_CONTENT,
@@ -17,9 +25,9 @@ const ContentPage = () => {
   )
   const navigate = useNavigate()
   const [listItems, setListItems] = useState([])
-  const [hasMore, setHasMore] = useState(true)
   const [nodes, setNodes] = useState(6)
 
+  // Redirect to login if unauthorized
   useEffect(() => {
     const statusCode = error?.networkError?.statusCode
     if (statusCode === ERR_CODE_UNAUTH) {
@@ -27,54 +35,72 @@ const ContentPage = () => {
     }
   }, [error, navigate])
 
+  // Fetch initial content
   useEffect(() => {
     fetchContent({ variables: { first: nodes } })
   }, [])
 
+  // Callback to fetch more content after a delay
   const fetchCb = () => {
     setTimeout(() => fetchContent({ variables: { first: nodes } }), 500)
   }
 
+  // Update listItems when lazyData changes
   useEffect(() => {
     const tempData = lazyData?.Admin?.Tree?.GetContentNodes?.edges
     if (!lazyData || !tempData) return
 
     setListItems(tempData)
     setNodes((prev) => prev + 4)
-    if (tempData.length > 0) {
-      setHasMore(true)
-    }
-    setHasMore(false)
   }, [lazyData])
 
+  // Function to handle tile movement
+  const moveTile = useCallback((dragIndex, hoverIndex) => {
+    setListItems((prevCards) =>
+      update(prevCards, {
+        $splice: [
+          [dragIndex, 1],
+          [hoverIndex, 0, prevCards[dragIndex]]
+        ]
+      })
+    )
+  }, [])
+
+  // Memoized content list to avoid unnecessary re-renders
   const contentList = useMemo(() => {
-    return listItems.map((content) => {
+    return listItems.map((content, idx) => {
       const {
         structureDefinition: { title },
         id
       } = content.node
 
-      return <ContentTile key={id} id={id} title={title} />
+      return (
+        <ContentTile
+          key={id}
+          id={id}
+          index={idx}
+          title={title}
+          moveTile={moveTile}
+        />
+      )
     })
-  }, [listItems])
+  }, [listItems, moveTile])
 
   return (
-    <InfiniteScroll
-      dataLength={listItems.length}
-      next={fetchCb}
-      hasMore={hasMore}
-    >
-      <Grid
-        bgColor="hotpink"
-        templateColumns="repeat(2, 1fr)"
-        gap={2}
-        p={4}
-        h="100%"
-        overflowY="auto"
-      >
-        {contentList}
-      </Grid>
-    </InfiniteScroll>
+    <DndProvider backend={HTML5Backend}>
+      <InfiniteScroll dataLength={listItems.length} next={fetchCb} hasMore>
+        <Grid
+          bgColor="hotpink"
+          templateColumns="repeat(2, 1fr)"
+          gap={2}
+          p={4}
+          h="100%"
+          overflowY="auto"
+        >
+          {contentList}
+        </Grid>
+      </InfiniteScroll>
+    </DndProvider>
   )
 }
 
--- src/pages/ContentPage/components/ContentTile/ContentTile.jsx
+++ src/pages/ContentPage/components/ContentTile/ContentTile.jsx
@@ -1,35 +1,115 @@
+import { useRef } from 'react'
+import { useDrag, useDrop } from 'react-dnd'
 import { Card, CardBody, Grid, GridItem, Heading, Text } from '@chakra-ui/react'
 
-const ContentTile = ({ title, id }) => {
+/**
+ * ContentTile Component
+ *
+ * This component represents a draggable content tile used within a grid layout.
+ * It displays a card with title and ID information.
+ * Users can drag and drop this tile within a grid.
+ *
+ * Props:
+ * @param {string} title - The title of the content tile.
+ * @param {string} id - The unique ID of the content tile.
+ * @param {number} index - The index of the tile within the grid.
+ * @param {function} moveTile - A function to handle the movement of the card within the grid.
+ */
+
+const style = {
+  cursor: 'move'
+}
+
+const ContentTile = ({ title, id, index, moveTile }) => {
+  const ref = useRef(null)
+
+  // Drop functionality
+  const [{ handlerId }, drop] = useDrop({
+    accept: 'GridItem',
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId()
+      }
+    },
+    hover(item, monitor) {
+      if (!ref.current) {
+        return
+      }
+      const dragIndex = item.index
+      const hoverIndex = index
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return
+      }
+      // Determine rectangle on screen
+      const hoverBoundingRect = ref.current?.getBoundingClientRect()
+      // Get vertical middle
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+      // Determine mouse position
+      const clientOffset = monitor.getClientOffset()
+      // Get pixels to the top
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top
+      // Only perform the move when the mouse has crossed half of the items height
+      // When dragging downwards, only move when the cursor is below 50%
+      // When dragging upwards, only move when the cursor is above 50%
+      // Dragging downwards
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return
+      }
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return
+      }
+      moveTile(dragIndex, hoverIndex)
+      item.index = hoverIndex
+    }
+  })
+
+  // Drag functionality
+  const [{ isDragging }, drag] = useDrag({
+    type: 'GridItem',
+    item: () => {
+      return { id, index }
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging()
+    })
+  })
+
+  const opacity = isDragging ? 0 : 1
+  drag(drop(ref))
+
   return (
-    <GridItem colSpan={1}>
-      <Card h="100%" minH="30vh">
-        <CardBody>
-          <Heading
-            as="h1"
-            textAlign="center"
-            fontSize={{ base: '2xl', md: '3xl' }}
-            mb="3rem"
-          >
-            {title}
-          </Heading>
-          <Grid templateColumns="repeat(12, 1fr)">
-            <GridItem colSpan={3}>
-              <Heading
-                as="h4"
-                textAlign="center"
-                fontSize={{ base: 'xl', md: '2xl' }}
-              >
-                Id:
-              </Heading>
-            </GridItem>
-            <GridItem display="flex" colSpan={9} alignItems="center">
-              <Text wordBreak={'break-all'}>{id}</Text>
-            </GridItem>
-          </Grid>
-        </CardBody>
-      </Card>
-    </GridItem>
+    <div ref={ref} style={{ ...style, opacity }} data-handler-id={handlerId}>
+      <GridItem colSpan={1} id={id} index={index}>
+        <Card h="100%" minH="30vh">
+          <CardBody>
+            <Heading
+              as="h1"
+              textAlign="center"
+              fontSize={{ base: '2xl', md: '3xl' }}
+              mb="3rem"
+            >
+              {title}
+            </Heading>
+            <Grid templateColumns="repeat(12, 1fr)">
+              <GridItem colSpan={3}>
+                <Heading
+                  as="h4"
+                  textAlign="center"
+                  fontSize={{ base: 'xl', md: '2xl' }}
+                >
+                  Id:
+                </Heading>
+              </GridItem>
+              <GridItem display="flex" colSpan={9} alignItems="center">
+                <Text wordBreak="break-all">{id}</Text>
+              </GridItem>
+            </Grid>
+          </CardBody>
+        </Card>
+      </GridItem>
+    </div>
   )
 }
 
